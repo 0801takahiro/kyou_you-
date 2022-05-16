491f179c81b395568e7777dd5411d4c7d4ec1b4e
0801takahiro <taka_m-.-m@docomo.ne.jp>
2022-05-16 05:52:29 +0000

    EC2用調整favoriteをreferencesからintegerへ

--- db/migrate/20220509020817_create_favorites.rb
+++ db/migrate/20220509020817_create_favorites.rb
@@ -1,10 +1,12 @@
 class CreateFavorites < ActiveRecord::Migration[5.2]
   def change
     create_table :favorites do |t|
-      t.references :user, foreign_key: true, null: false
-      t.references :impression, foreign_key: true, null: false
+      t.integer :user_id, null: false
+      t.integer :impression_id, null: false
 
       t.timestamps
     end
+    add_foreign_key :favorites, :users
+    add_foreign_key :favorites, :impressions
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -33,8 +33,6 @@ ActiveRecord::Schema.define(version: 2022_05_10_172413) do
     t.integer "impression_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["impression_id"], name: "index_favorites_on_impression_id"
-    t.index ["user_id"], name: "index_favorites_on_user_id"
   end
 
   create_table "impressions", force: :cascade do |t|
